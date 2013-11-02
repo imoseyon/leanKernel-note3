301f612906a4094e29798e6fcb6c94a1e9dd4526
imoseyon <imoseyon@gmail.com>
2013-11-01 18:23:30 -0700

    ramdisk: /data/data/leankernel/paneltemp to control panel temperature
    * valid values are -60 to 0

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -1,11 +1,20 @@
 #!/system/bin/sh
 
+[ ! -d "/data/data/leankernel" ] && mkdir /data/data/leankernel
+chmod 755 /data/data/leankernel
+#
+# panel temperature setting
+#
+PANELTMP="/data/data/leankernel/paneltemp"
+PANELTMPSF="/sys/devices/qcom,mdss_dsi_samsung_octa_1080p_cmd.71/lcd/panel/temperature"
+[ -f $PANELTMP ] && echo `cat $PANELTMP` > $PANELTMPSF
+
+# daemonsu support - probably not needed
 [ -f "/system/xbin/daemonsu" ] && /system/xbin/daemonsu --auto-daemon &
 
 pm disable com.sec.knox.seandroid
 
 /system/xbin/busybox run-parts /system/etc/init.d
-/system/xbin/busybox run-parts /sbin/init.d
 
 stop thermal-engine
 sleep 2
