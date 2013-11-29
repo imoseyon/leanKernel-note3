4e4685e94efb3d6639b6a8de2b713e5142bf2768
imoseyon <imoseyon@gmail.com>
2013-11-29 09:33:10 -0800

    Revert "touchscreen: use system_wq instead of dedicated workqueues"
    This reverts commit 850b76b800783e00352a97cd25d860794dc0c077.

--- drivers/input/touchscreen/synaptics/rmi_f54.c
+++ drivers/input/touchscreen/synaptics/rmi_f54.c
@@ -1238,6 +1238,7 @@ struct synaptics_rmi4_f54_handle {
 	struct hrtimer watchdog;
 	struct work_struct timeout_work;
 	struct delayed_work status_work;
+	struct workqueue_struct *status_workqueue;
 	struct synaptics_rmi4_exp_fn_ptr *fn_ptr;
 	struct synaptics_rmi4_data *rmi4_data;
 };
@@ -1971,7 +1972,9 @@ static void timeout_set_status(struct work_struct *work)
 					__func__);
 			f54->status = STATUS_ERROR;
 		} else {
-			schedule_delayed_work(&f54->status_work, 0);
+			queue_delayed_work(f54->status_workqueue,
+					&f54->status_work,
+					0);
 			mutex_unlock(&f54->status_mutex);
 			return;
 		}
@@ -5531,7 +5534,8 @@ static void synaptics_rmi4_f54_attn(struct synaptics_rmi4_data *rmi4_data,
 		return;
 
 	if (f54->intr_mask & intr_mask) {
-		schedule_delayed_work(&f54->status_work,
+		queue_delayed_work(f54->status_workqueue,
+				&f54->status_work,
 				msecs_to_jiffies(STATUS_WORK_INTERVAL));
 	}
 
@@ -5787,6 +5791,8 @@ pdt_done:
 	f54->factory_data = factory_data;
 #endif
 
+	f54->status_workqueue =
+		create_singlethread_workqueue("f54_status_workqueue");
 	INIT_DELAYED_WORK(&f54->status_work,
 			synaptics_rmi4_f54_status_work);
 
@@ -5861,6 +5867,8 @@ static void synaptics_rmi4_f54_remove(struct synaptics_rmi4_data *rmi4_data)
 #endif
 
 	cancel_delayed_work_sync(&f54->status_work);
+	flush_workqueue(f54->status_workqueue);
+	destroy_workqueue(f54->status_workqueue);
 
 #ifdef FACTORY_MODE
 	sysfs_remove_group(&f54->factory_data->fac_dev_ts->kobj, &cmd_attr_group);
