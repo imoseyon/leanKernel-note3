d63be2b30d6872cb6bed69482a2c12a315090647
imoseyon <imoseyon@gmail.com>
2014-01-14 23:36:23 -0800

    Fix merge errors

--- include/sound/core.h
+++ include/sound/core.h
@@ -300,6 +300,8 @@ int snd_card_info_done(void);
 int snd_component_add(struct snd_card *card, const char *component);
 int snd_card_file_add(struct snd_card *card, struct file *file);
 int snd_card_file_remove(struct snd_card *card, struct file *file);
+void snd_card_change_online_state(struct snd_card *card, int online);
+bool snd_card_is_online_state(struct snd_card *card);
 void snd_card_unref(struct snd_card *card);
 
 #define snd_card_set_dev(card, devptr) ((card)->dev = (devptr))
