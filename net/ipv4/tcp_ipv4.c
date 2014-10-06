cfd1499daadb7fce31dc29082d4c712f98c46296
arter97 <qkrwngud825@gmail.com>
2014-10-07 02:39:54 +0900

    Merge Samsung release N9005XXUFNG2 (Update 9)
    SM-N9005_KK_Opensource_Update9.zip
    
     - Properly merge mdss changes
     - Fix previously durped fs/exfat (during Update 8)
     - After batch merging with 'patch -p1',
       all changes were validated with human eyes to remove
       duplicates(e.g. kernel/futex.c)
       and mismatched functions(e.g. sound/soc/soc-dapm.c)
    
    Change-Id: I923889f048b4a0984e73c1eaec79851f6bda7f78
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    
    Conflicts:
    	drivers/misc/sec_misc.c
    	drivers/net/wireless/bcmdhd/Makefile
    	drivers/video/msm/mdss/mdss_samsung_dsi_panel.c
    	fs/buffer.c
    	net/ipv4/ping.c
    	net/netlink/af_netlink.c

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1668,7 +1668,9 @@ int tcp_v4_rcv(struct sk_buff *skb)
 	struct sock *sk;
 	int ret;
 	struct net *net = dev_net(skb->dev);
+#if !defined(CONFIG_SEC_LOCALE_CHN)
 	struct in_device *in_dev;
+#endif
 
 	if (skb->pkt_type != PACKET_HOST)
 		goto discard_it;
@@ -1759,6 +1761,9 @@ no_tcp_socket:
 bad_packet:
 		TCP_INC_STATS_BH(net, TCP_MIB_INERRS);
 	} else {
+#if defined(CONFIG_SEC_LOCALE_CHN)
+		tcp_v4_send_reset(NULL, skb);
+#else
 		in_dev = in_dev_get(skb->dev);
 		if (in_dev) {
 			if (!IN_DEV_FORWARD(in_dev))
@@ -1766,6 +1771,7 @@ bad_packet:
 			in_dev_put(in_dev);
 		} else
 			tcp_v4_send_reset(NULL, skb);
+#endif
 	}
 
 discard_it:
